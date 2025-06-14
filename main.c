ec2b059eb89f9a0fe4f28e422dc50f207cd2aa6c
HALO444 <madhushanka.halo44o@gmail.com>
2025-06-14 19:53:46 +0530

    digits() testing 1

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void digits();
+
 int main(void)
 {
-    //
+  digits();
+}
+
+void digits()
+{
+    int user_input;
+    printf("How many digits(2by2 or 3by3) : ");
+    scanf("%d", &user_input);
+    if (user_input > 3)
+    {
+        digits();
+    }
+
 }
