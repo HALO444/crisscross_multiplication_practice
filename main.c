d1ee4054668c8a3cd7a864d1de69a368d0c42684
HALO444 <madhushanka.halo44o@gmail.com>
2025-06-14 20:04:15 +0530

    digits() done

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
-void digits();
+int digits(void);
 
 int main(void)
 {
   digits();
 }
 
-void digits()
+int digits(void)
 {
     int user_input;
     printf("How many digits(2by2 or 3by3) : ");
     scanf("%d", &user_input);
-    if (user_input > 3)
+    if (user_input >= 3 || user_input <= 0)
     {
         digits();
     }
+    make_number(user_input);
+}
+
+void make_number(int user_input)
+{
 
 }
